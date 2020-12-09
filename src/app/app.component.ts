66cd29fe8a2068c1c41e7003d1b27b001a163fd8
Gary Jackson <gary.jackson@halewood-int.com>
2020-12-09 09:06:51 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  
+  ngOnInit() : void {
+    window.parent.postMessage({type:'initialized'}, "*")
+
+    window.parent.postMessage({type:'sizeChange', data: {height: 400}}, "*")
+  }
+
+
+}
