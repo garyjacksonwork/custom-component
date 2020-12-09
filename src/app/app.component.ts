fa1ef1c58c3fb3baae2aaa6a5f04c25c3c706b20
Gary Jackson <gary.jackson@halewood-int.com>
2020-12-09 09:40:48 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  
-  /*
-  ngOnInit() : void {
-    window.parent.postMessage({type:'initialized'}, "*")
-
-    window.parent.postMessage({type:'sizeChange', data: {height: 400}}, "*")
-  }
-*/
-
+export class AppComponent {
 }
